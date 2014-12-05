a41e3c3a43b9f9c182aeb3cf7f5c08a44267e912
Iuli Dercaci <iuli.dercaci@binasystems.com>
2014-12-05 13:48:45 +0200

    token acquiring condition fix

--- src/B2B/BlogBundle/EventListener/TokenListener.php
+++ src/B2B/BlogBundle/EventListener/TokenListener.php
@@ -1,7 +1,6 @@
 <?php
 namespace B2B\BlogBundle\EventListener;
 
-use Acme\DemoBundle\Controller\TokenAuthenticatedController;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -20,14 +19,13 @@ class TokenListener
     public function onKernelController()
     {
         $session = $this->request->getSession();
-//        if (!$session->get('auth') && isset($session->get('auth')['token'])) {
+        if (!$session->get('auth') || !isset($session->get('auth')['token'])) {
             $config = $this->container->getParameter('api');
             $params = json_encode([
                 'username' => $config['login'],
                 'password' => $config['password'],
                 'mac_addr' => $config['mac_addr']
             ]);
-        var_dump($params);
             /**
              * @var $request \Buzz\Browser
              */
@@ -41,10 +39,8 @@ class TokenListener
                 throw new \Exception ("Authentication error");
             }
             $responseData = json_decode($response->getContent(), true);
-            var_dump($response->getContent());
-            exit;
 
             $session->set('auth', ['token' => $responseData['results']['token']]);
-//        }
+        }
     }
 }
